9a2f603d2d221560ef314b1b1b3bf53abfa14089
harin <harini.mkumar1@gmail.com>
2026-02-07 04:27:55 +0530

    v1

---
+++ render/renderer.py
@@ -0,0 +1,19 @@
+#---viz---#
+
+
+import matplotlib.pyplot as plt
+
+from grid import Grid
+
+class Renderer:
+    
+    def __init__(self, grid: Grid):
+        self.fig, self.ax = plt.subplots()
+        self.im = self.ax.imshow(grid.data, cmap = 'binary')
+        self.ax.set_xticks([])
+        self.ax.set_yticks([])
+        pass
+
+    def draw(self, grid:Grid):
+        self.im.set_data(grid.data)
+        plt.pause(0.1)
---
+++ simulation/engine.py
@@ -0,0 +1,20 @@
+#---time evoltion observation---#
+
+from core.stepper import step
+from core.grid import Grid
+from world.boundaries import Boundary
+
+
+class Simulation:
+
+    def __init__(self, grid: Grid, boundary: Boundary):
+        self.grid = grid
+        self.boundary = boundary
+        self.t = 0
+
+
+    def tick(self):
+        counts = self.boundary.count_neighbors(self.grid.data)
+        self.grid = step(self.grid, counts)
+        self.t += 1
+        return self.grid
